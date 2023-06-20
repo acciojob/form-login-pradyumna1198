ce4968c79c18f237bc52ec3f189601eda1064653
pradyumna1198 <92784304+pradyumna1198@users.noreply.github.com>
2023-06-20 22:44:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	var firstName = document.getElementById('form1').elements.namedItem('fname').value;
 
+	var lastName = document.getElementById('form1').elements.namedItem('lname').value;
+
+	alert(firstName + ' ' + lastName);
 }
